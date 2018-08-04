0bb304d16e1723da47b8f4d8484c39da4ee1136c
Laserzwei <thembaandcoproductions@gmx.de>
2018-08-04 21:23:37 +0200

    1.7.2

--- mods/CarrierCommander/config/CarrierCommanderConfig.lua
+++ mods/CarrierCommander/config/CarrierCommanderConfig.lua
@@ -3,7 +3,7 @@ local Config = {}
 Config.Author = "Nexus, Dirtyredz, Hammelpilaw, Maxx4u, Laserzwei"
 Config.ModName = "CarrierCommander"
 Config.version = {
-    major=1, minor=7, patch = 1,
+    major=1, minor=7, patch = 2,
     string = function()
         return  Config.version.major .. '.' ..
                 Config.version.minor .. '.' ..
