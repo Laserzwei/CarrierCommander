204c8dcc24d4f847358ee5e1a9b320930a7cbddb
Laserzwei <lasernr2@gmail.com>
2019-06-03 16:16:50 +0200

    0.23.*

--- CC Basic Commands/modinfo.lua
+++ CC Basic Commands/modinfo.lua
@@ -40,7 +40,7 @@ meta =
     -- },
     dependencies = {
         {id = "1741735681", min = "1.12"},
-        {id = "Avorion", min = "0.23"}
+        {id = "Avorion", min = "0.23.*"}
     },
 
     -- Set to true if the mod only has to run on the server. Clients will get notified that the mod is running on the server, but they won't download it to themselves
