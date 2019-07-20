12b1dc33e0ede0a2340e545c257a9d8128870671
Laserzwei <lasernr2@gmail.com>
2019-07-20 12:06:41 +0200

    fixed script duplication bugged caused by canilla misbehaviour

--- Carrier Commander/data/scripts/entity/init.lua
+++ Carrier Commander/data/scripts/entity/init.lua
@@ -3,7 +3,9 @@ if onServer() then
 local entity = Entity()
 if entity.isShip or entity.isStation then
     if entity.allianceOwned or entity.playerOwned then
-        entity:addScriptOnce("data/scripts/entity/CarrierCommander.lua")
+        if not entity:hasScript("CarrierCommander.lua") then
+            entity:addScriptOnce("data/scripts/entity/CarrierCommander.lua")
+        end
     end
 end
 end
